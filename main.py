263c84d54f343b2cb70e49f61b334d53488d6a27
refael <a0504865888@gmail.com>
2026-03-05 16:36:31 +0200

    16:39

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print ("hello world")
+
+def grret(name):
+    print(f"hello, {name}!")
+
+
+
+grret("refael")
+grret("shi")
+
+
+def add(num1, num2):
+    print(num1 + num2)
+
+
+add(3,3)
+add(5,5)
+
