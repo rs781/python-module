f83f15158d42d0dda1e18523440f2b2207175f9d
refael <a0504865888@gmail.com>
2026-03-05 18:40:00 +0200

    funkzia

---
+++ test.py
@@ -0,0 +1,43 @@
+def say_hello():
+    print("hello python")
+
+say_hello()
+
+
+def print_number(parameter):
+    print (parameter)
+
+print_number(7)
+
+
+def double_number(num):
+    print(num * 2)
+
+double_number(7)
+    
+def add_numbers(num1, num2):
+    print(num1 + num2)
+
+
+add_numbers(6,9)
+
+
+def print_name(name):
+    print(f"hello,{name}!")
+
+print_name("rfi")
+print_name("simi")
+print_name("goli")
+    
+
+def square(nume):
+    return nume*4
+
+result=square(6)
+print(result)
+
+
+def is_even()
+
+
+
